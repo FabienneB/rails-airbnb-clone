9606e0eadd07e6b3c4079de4714d357235629e3a
Fabienne Bremond <fabienne.bremond@gmail.com>
2017-08-15 19:26:25 +0200

    ajout validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,12 @@ class Booking < ApplicationRecord
   belongs_to :cowork
   validates :checkin, presence: true
   validates :checkout, presence: true
-end
+  validate :checkout_after_checkin
 
+
+  def checkout_after_checkin
+    if checkout < checkin
+      errors.add(:checkout, "The check-out date can't be before check-in")
+    end
+  end
+end
