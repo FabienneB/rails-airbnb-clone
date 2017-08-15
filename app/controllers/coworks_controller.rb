daf57ddb560d1ddb4f978a240cefdf168a2e18dd
Elodie Lachoua <elodie.lachoua@gmail.com>
2017-08-15 12:08:53 +0200

    cowork controller

--- app/controllers/coworks_controller.rb
+++ app/controllers/coworks_controller.rb
@@ -1,16 +1,17 @@
 class CoworksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :index]
   def index
-
+    @coworks = Cowork.all
   end
 
   def show
+    @cowork = cowork.find(cowork_params)
     @booking = Booking.new
   end
 
 
-  def product_params
-  params.require(:cowork).permit(:city, :capacity, :user_id, photos: [])
+  def cowork_params
+    params.require(:cowork).permit(:city, :capacity, :user_id, photos: [])
   end
 end
 
