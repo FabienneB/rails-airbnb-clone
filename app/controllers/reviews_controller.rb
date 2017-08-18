c038a86ae73070e209c575ab60145f5ba4c074ff
Elodie Lachoua <elodie.lachoua@gmail.com>
2017-08-18 12:16:21 +0200

    end controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,17 @@
+class ReviewsController < ApplicationController
+
+  # def create
+  #   @cowork = Corwork.find(params[:cowork_id])
+  #   @review = Review.new(review_params)
+  #   review.cowork = @cowork
+  #   if @cowork.save
+  #     redirect_to
+
+  # end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+end
+end
