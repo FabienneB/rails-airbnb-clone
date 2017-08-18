24921add7389930aad7152b31281ee7e0e32d2ca
Fabienne Bremond <fabienne.bremond@gmail.com>
2017-08-18 09:38:47 +0200

    correction erreur dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,4 @@
-class CoworksController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show, :index, :new]
-
+class DashboardController < ApplicationController
   def index
     @coworks = Cowork.where(user_id: current_user.id)
   end
