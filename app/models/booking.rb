5da32d1da7c439eeb9be520be0b202e1ccf73e64
Fabienne Bremond <fabienne.bremond@gmail.com>
2017-11-02 19:03:50 +0100

    ajout de la condition de validation nbre de coworkers

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,9 +6,18 @@ class Booking < ApplicationRecord
   validates :nbr_coworkers, presence: true
   validates :nbr_coworkers, numericality: { only_integer: true }
   validate :checkout_after_checkin
+  validate :sum_coworkers
 
 
   def checkout_after_checkin
     errors.add(:checkout, "The check-out can't be before the check-in") if checkout < checkin
   end
+
+
+  def sum_coworkers
+    errors.add(:nbr_coworkers, "Sorry but the numbers of coworkers exceeded the capacity of this kikoho place") if nbr_coworkers > (self.cowork.capacity - self.cowork.bookings.where(:status == "accepted").sum { |nb| nb.nbr_coworkers })
+  end
+
 end
+
+
